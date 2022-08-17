47db49e2f82889a6bbef1d1ff3323423d2a54c27
tabata Vasques <tabata07defaria@gmail.com>
2022-08-17 09:42:40 -0300

    first commit

---
+++ conversor.js
@@ -0,0 +1,13 @@
+function resultCm(){
+    var n1 = document.getElementById('n1')
+    var n2 = document.getElementById('n2') 
+    n2.value = Number(n1.value )* 100
+
+}
+
+function resultMetro(){
+    var n1 = document.getElementById('n1')
+    var n2 = document.getElementById('n2')
+    n1.value = Number(n2.value )/ 100
+
+}
